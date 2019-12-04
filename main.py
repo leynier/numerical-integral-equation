7e97f72cccc556cede7c0d620fab75f1ef95e750
leynier <leynier41@gmail.com>
2019-12-04 09:35:44 -0500

    Add samples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from matplotlib.pyplot import plot, show, title
+from sys import argv
+from matplotlib.pylab import figure, plot, show, subplot, title, xlabel
 from scipy import array, linspace, ndarray
 from scipy.interpolate import CubicSpline
 from sympy import sympify, SympifyError
@@ -49,6 +50,31 @@ def get_analytical_form(dim2 = False):
 
 if __name__ == "__main__":
     try:
+        if len(argv) > 1 and argv[1] == 'samples':
+            print('Sample 1:')
+            K1 = lambda x, y: sympify('1 + x * y').evalf(subs={'x': x, 'y': y})
+            f1 = lambda x: sympify('1').evalf(subs={'x': x})
+            a1 = -1
+            b1 = 1
+            print(f'K(x, y) = {K1}')
+            print(f'f(x) = {f1}')
+            print(f'Interval = [{a1}, {b1}]')
+            solve1_1, cond1_1 = integral_equation(K1, f1, a1, b1, use_python_libs=True)
+            solve1_2, cond1_2 = integral_equation(K1, f1, a1, b1, use_python_libs=False)
+            x1 = linspace(a1, b1)
+            y1_1 = solve1_1(x1)
+            y1_2 = solve1_2(x1)
+            figure('Sample 1')
+            subplot(1, 2, 1)
+            plot(x1, y1_1)
+            title(f'Sample 1: Use Python Libraries')
+            xlabel(f'Matrix condition: {cond1_1}')
+            subplot(1, 2, 2)
+            plot(x1, y1_2)
+            title(f'Sample 1: NO use Python Libraries')
+            xlabel(f'Matrix condition: {cond1_2}')
+            show()
+            exit()
         option = int(input('Insert 1 if use Python libraries or 2 if no: '))
         if option != 1 and option != 2:
             print_error('Error: The number of option should be 1 or 2.')
